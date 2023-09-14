6bcd38b8d1eea7899d63696b0174fc4e6b9d8455
Vic <tienthanh@inspectorio.com>
2023-09-14 16:23:06 +0700

    update

--- src/components/PaginationComponent.vue
+++ src/components/PaginationComponent.vue
@@ -1,7 +1,5 @@
 <template>
-  <div
-    class="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6 mt-5"
-  >
+  <div class="flex items-center justify-between border-t border-gray-200 bg-white py-3 mt-5">
     <div class="flex flex-1 justify-between sm:hidden">
       <a
         href="#"
